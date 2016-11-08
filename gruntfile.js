b0f31027fe72b389876d1e4b73f7373e3e036d36
Sarah Lamont <sarah@sarahlamont.com>
2016-11-08 11:30:27 -0500

    modifying the build script

--- gruntfile.js
+++ gruntfile.js
@@ -6,26 +6,73 @@ module.exports = function(grunt){
 				separator: '\n\n//---------------------------\n\n'
 			},
 			dist : { //name of job for that task
-				src: ['dev/scripts/*.js'],
-				dest: 'dist/js/script.js'
+				src: ['assets/js/script/*.js'],
+				dest: 'assets/js/script.js'
 			}
 		},//concat
 
 		bower_concat:{
 			all: {
-				dest: 'dist/js/_bower.js',
-				cssDest: 'dist/css/_bower.css'
+				dest: 'assets/js/_bower.js',
+				cssDest: 'assets/css/_bower.css'
 			}
 		},
-
+		uglify: {
+			options: {
+  		  		mangle: false,
+  		  		sourceMap: true
+  		  	},
+  		  	target:{
+  		  		files: {
+	  		    	'dist/assets/js/script.min.js': ['assets/js/_bower.js','assets/js/script.js']
+	  			}
+  		  	}
+		},
+		cssmin: {
+		  options: {
+		    shorthandCompacting: false,
+		    roundingPrecision: -1
+		  },
+		  target: {
+		    files: {
+		      'dist/assets/css/style.min.css': ['assets/css/_bower.css', 'assets/css/style.css']
+		    }
+		  }
+		},
+		copy: {
+		  main: {
+		    files: [{
+		      	expand: true, 
+		      	flatten: false, 
+		      	cwd: 'assets/images',
+    			src: '**',
+		      	dest: 'dist/assets/images', 
+		      	filter: 'isFile'
+		      }]
+		  }
+		},
+		processhtml: {
+		    options: {},
+		    dist: {
+		      files: [
+		          {
+		          expand: true,     
+		          cwd: '',   
+		          src: ['*.html'],
+		          dest: 'dist/',  
+		          ext: '.html'
+		        },
+		        ]
+		    }
+  		},
 		sass: {
 			dist: {
 				options: { //look at plugin website for various options
 					style: 'expanded'
 				},
 				files: [{
-					src: 'dev/sass/style.scss',
-					dest: 'dist/css/style.css'
+					src: 'assets/sass/style.scss',
+					dest: 'assets/css/style.css'
 				}]
 			}
 		}, //sass
@@ -35,7 +82,7 @@ module.exports = function(grunt){
 				options: {
 					hostname: 'localhost',
 					port: 3000,
-					base: 'dist/',
+					base: '',
 					livereload: true
 				}
 			}
@@ -47,20 +94,25 @@ module.exports = function(grunt){
 				livereload: true
 			},
 			scripts: {
-				files: ['dist/**/*.html', 
-				'dev/scripts/**/*.js',
-				'dev/sass/**/*.scss'],
+				files: ['**/*.html', 
+				'assets/scripts/**/*.js',
+				'assets/sass/**/*.scss'],
 				tasks: ['concat','sass']
 			}
 		}
 
 	}); //init config section
-
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-processhtml');
 	grunt.loadNpmTasks('grunt-contrib-concat'); //concatenates files
 	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.loadNpmTasks('grunt-bower-concat');
 	grunt.registerTask('default', ['bower_concat', 'concat', 'sass', 'connect', 'watch']);
+	grunt.registerTask('dist', ['bower_concat', 'concat', 'sass', 'copy', 'uglify', 'cssmin', 'processhtml']);
+
 
 }; //wrapper function
